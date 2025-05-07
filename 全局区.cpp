818cc318ff836636906b227f999408e27a13bdd8
Eliauk-qwe <1552356639@qq.com>
2025-05-07 21:06:02 +0800

    Add files via upload

---
+++ 全局区.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using  namespace std;
+//全局区   全局变量  
+//        静态变量   
+//        常量  字符串常量  
+//             const修饰变量   全局变量
+//                            局部变量     XXXXX
+int q_a=10;
+int q_b=10;
+
+const int c_q_a=10;
+const int c_q_b=10;
+
+
+int main(){
+
+    int a=10;
+    int b=10;
+
+    static int s_a=10;
+    static int s_b=10;
+
+    char* s="hello";
+
+
+    const int c_j_a=10;
+    const int c_j_b=10;
+
+    //打印地址看看效果
+    //
+
+    //
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+}
