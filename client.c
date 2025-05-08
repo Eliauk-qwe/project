9280fa06950b703d80d467031a7fa73e011de8f6
Eliauk-qwe <1552356639@qq.com>
2025-05-08 21:27:00 +0800

    Add files via upload

--- client.c
+++ client.c
@@ -1,46 +1,137 @@
-#include "inet_sockets.h"
-#include "tlpi_hdr.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 
-#define BUF_SIZE 100
+#define CONTROL_PORT 2100
+#define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[])
-{
-    int sfd;
-    ssize_t numRead;
-    char buf[BUF_SIZE];
+// 连接到FTP服务器并发送命令
+int connect_to_server(const char *server_ip) {
+    int control_socket;
+    struct sockaddr_in server_addr;
 
-    if (argc!= 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s host\n", argv[0]);
+    control_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (control_socket < 0) {
+        perror("Error creating control socket");
+        return -1;
+    }
 
-    sfd = inetConnect(argv[1], "echo", SOCK_STREAM);
-    if (sfd == -1)
-        errExit("inetConnect");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(CONTROL_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    switch (fork()) {
-    case -1:
-        errExit("fork");
+    if (connect(control_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error connecting to server");
+        close(control_socket);
+        return -1;
+    }
 
-    case 0:         /* Child: read server's response, echo on stdout */
-        for (;;) {
-            numRead = read(sfd, buf, BUF_SIZE);
-            if (numRead <= 0)         /* Exit on EOF or error */
-                break;
-            printf("%.*s", (int) numRead, buf);
-        }
-        exit(EXIT_SUCCESS);
-
-    default:        /* Parent: write contents of stdin to socket */
-        for (;;) {
-            numRead = read(STDIN_FILENO, buf, BUF_SIZE);
-            if (numRead <= 0)         /* Exit loop on EOF or error */
-                break;
-            if (write(sfd, buf, numRead)!= numRead)
-                fatal("write() failed");
-        }
-
-        /* Close writing channel, so server sees EOF */
-        if (shutdown(sfd, SHUT_WR) == -1)
-        errExit("shutdown");
-exit(EXIT_SUCCESS);
+    return control_socket;
 }
+
+// 发送PASV命令并解析返回的端口号
+int enter_passive_mode(int control_socket) {
+    char buffer[BUFFER_SIZE];
+    int p1, p2, data_port;
+
+    send(control_socket, "PASV\r\n", 6, 0);
+    recv(control_socket, buffer, BUFFER_SIZE, 0);
+    printf("Server response: %s", buffer);
+
+    // 从服务器响应中提取端口号
+    if (sscanf(buffer, "227 Entering Passive Mode (%*d,%*d,%*d,%*d,%d,%d)", &p1, &p2) == 2) {
+        data_port = p1 * 256 + p2;
+        printf("Data port: %d\n", data_port);
+    } else {
+        return -1;
+    }
+
+    return data_port;
+}
+
+// 启动数据连接并进行文件传输
+void transfer_file(int control_socket, const char *filename, int is_upload) {
+    int data_socket;
+    struct sockaddr_in data_addr;
+    FILE *file;
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+
+    // 创建数据传输的socket
+    data_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (data_socket < 0) {
+        perror("Error creating data socket");
+        return;
+    }
+
+    data_addr.sin_family = AF_INET;
+    data_addr.sin_port = htons(enter_passive_mode(control_socket));  // 获取数据端口
+    data_addr.sin_addr.s_addr = inet_addr("192.168.1.1");  // 使用服务器的IP
+
+    if (connect(data_socket, (struct sockaddr *)&data_addr, sizeof(data_addr)) < 0) {
+        perror("Error connecting to data port");
+        close(data_socket);
+        return;
+    }
+
+    if (is_upload) {
+        // 上传文件
+        file = fopen(filename, "rb");
+        if (file == NULL) {
+            perror("Error opening file for upload");
+            close(data_socket);
+            return;
+        }
+
+        // 读取文件并发送数据
+        while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+            send(data_socket, buffer, bytes_read, 0);
+        }
+
+        printf("File uploaded successfully.\n");
+        fclose(file);
+    } else {
+        // 下载文件
+        file = fopen(filename, "wb");
+        if (file == NULL) {
+            perror("Error opening file for download");
+            close(data_socket);
+            return;
+        }
+
+        // 接收数据并写入文件
+        while ((bytes_read = recv(data_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+            fwrite(buffer, 1, bytes_read, file);
+        }
+
+        printf("File downloaded successfully.\n");
+        fclose(file);
+    }
+
+    close(data_socket);
+}
+
+// 主函数
+int main() {
+    const char *server_ip = "192.168.1.1";
+    int control_socket = connect_to_server(server_ip);
+    if (control_socket == -1) return -1;
+
+    // 发送PASV命令并获取数据端口
+    int data_port = enter_passive_mode(control_socket);
+    if (data_port == -1) {
+        printf("Error entering passive mode\n");
+        close(control_socket);
+        return -1;
+    }
+
+    // 文件上传或下载
+    transfer_file(control_socket, "file.txt", 1);  // 上传文件
+    // transfer_file(control_socket, "file.txt", 0);  // 下载文件
+
+    close(control_socket);
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,32 +1,221 @@
-#include "us_xfr.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <dirent.h>
 
-int main(int argc, char *argv[])
-{
-    struct sockaddr_un addr;
-    int sfd;
-    ssize_t numRead;
-    char buf[BUF_SIZE];
+#define CONTROL_PORT 2100
+#define BUFFER_SIZE 1024
 
-    sfd = socket(AF_UNIX, SOCK_STREAM, 0);    /* Create client socket */
-    if (sfd == -1)
-        errExit("socket");
+// 用于存储客户端信息
+typedef struct {
+    int control_socket;
+    struct sockaddr_in client_addr;
+} client_info_t;
 
-    /* Construct server address, and make the connection */
-    memset(&addr, 0, sizeof(struct sockaddr_un));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1);
+// 处理文件列表的函数
+void send_file_list(int data_socket) {
+    DIR *dir;
+    struct dirent *entry;
+    char buffer[BUFFER_SIZE];
 
-    if (connect(sfd, (struct sockaddr *) &addr,
-                sizeof(struct sockaddr_un)) == -1)
-        errExit("connect");
+    dir = opendir(".");
+    if (dir == NULL) {
+        perror("Error opening directory");
+        return;
+    }
 
-    /* Copy stdin to socket */
-while ((numRead = read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
-    if (write(sfd, buf, numRead) != numRead)
-        fatal("partial/failed write");
+    // 读取并发送目录中的文件列表
+    while ((entry = readdir(dir)) != NULL) {
+        snprintf(buffer, sizeof(buffer), "%s\r\n", entry->d_name);
+        send(data_socket, buffer, strlen(buffer), 0);
+    }
 
-if (numRead == -1)
-    errExit("read");
+    closedir(dir);
+}
 
-exit(EXIT_SUCCESS);
-    /* Closes our socket; server sees EOF */
+// 处理文件上传的函数
+void receive_file(int data_socket, const char *filename) {
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+    int file_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (file_fd == -1) {
+        perror("Error opening file for writing");
+        return;
+    }
+
+    // 接收文件内容并写入文件
+    while ((bytes_received = recv(data_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+        write(file_fd, buffer, bytes_received);
+    }
+
+    close(file_fd);
+    printf("File uploaded successfully.\n");
+}
+
+// 处理文件下载的函数
+void send_file(int data_socket, const char *filename) {
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+    int file_fd = open(filename, O_RDONLY);
+    if (file_fd == -1) {
+        perror("Error opening file for reading");
+        return;
+    }
+
+    // 读取文件并发送内容
+    while ((bytes_read = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
+        send(data_socket, buffer, bytes_read, 0);
+    }
+
+    close(file_fd);
+    printf("File downloaded successfully.\n");
+}
+
+// 启动一个新线程来处理客户端连接
+void *handle_client(void *client_sock) {
+    int control_socket = *((int *) client_sock);
+    char buffer[BUFFER_SIZE];
+    char *response;
+
+    // 发送欢迎消息
+    send(control_socket, "220 Welcome to FTP server\r\n", 26, 0);
+
+    // 进入通信循环
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recv(control_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received <= 0) {
+            break;
+        }
+
+        printf("Received command: %s", buffer);
+
+        if (strncmp(buffer, "PASV", 4) == 0) {
+            // 生成一个随机端口用于数据传输
+            int data_port = rand() % 64511 + 1024;  // 1024到65535之间
+            int p1 = data_port / 256;
+            int p2 = data_port % 256;
+            response = (char *) malloc(50);
+            sprintf(response, "227 Entering Passive Mode (192,168,1,1,%d,%d)\r\n", p1, p2);
+            send(control_socket, response, strlen(response), 0);
+            free(response);
+
+            // 启动一个新的线程来处理数据传输
+            pthread_t data_thread;
+            pthread_create(&data_thread, NULL, handle_data_connection, (void *) &data_port);
+            pthread_detach(data_thread);
+        } else if (strncmp(buffer, "LIST", 4) == 0) {
+            // 创建数据传输线程来处理文件列表
+            pthread_t data_thread;
+            pthread_create(&data_thread, NULL, handle_data_connection, (void *) "LIST");
+            pthread_detach(data_thread);
+        } else if (strncmp(buffer, "STOR", 4) == 0) {
+            // 创建数据传输线程来处理文件上传
+            pthread_t data_thread;
+            pthread_create(&data_thread, NULL, handle_data_connection, (void *) "STOR");
+            pthread_detach(data_thread);
+        } else if (strncmp(buffer, "RETR", 4) == 0) {
+            // 创建数据传输线程来处理文件下载
+            pthread_t data_thread;
+            pthread_create(&data_thread, NULL, handle_data_connection, (void *) "RETR");
+            pthread_detach(data_thread);
+        } else {
+            send(control_socket, "500 Unknown command\r\n", 21, 0);
+        }
+    }
+
+    close(control_socket);
+    return NULL;
+}
+
+// 处理数据传输连接
+void *handle_data_connection(void *data_port_ptr) {
+    int data_socket;
+    struct sockaddr_in data_addr;
+    const char *command = (const char *) data_port_ptr;
+
+    // 创建数据传输socket
+    data_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (data_socket == -1) {
+        perror("Error creating data socket");
+        return NULL;
+    }
+
+    data_addr.sin_family = AF_INET;
+    data_addr.sin_port = htons(2101);  // 示例数据端口
+    data_addr.sin_addr.s_addr = inet_addr("192.168.1.1");
+
+    if (connect(data_socket, (struct sockaddr *)&data_addr, sizeof(data_addr)) < 0) {
+        perror("Error connecting to data port");
+        return NULL;
+    }
+
+    if (strcmp(command, "LIST") == 0) {
+        send_file_list(data_socket);
+    } else if (strcmp(command, "STOR") == 0) {
+        receive_file(data_socket, "uploaded_file.txt");
+    } else if (strcmp(command, "RETR") == 0) {
+        send_file(data_socket, "downloaded_file.txt");
+    }
+
+    close(data_socket);
+    return NULL;
+}
+
+// 启动FTP服务器
+void start_ftp_server() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    pthread_t client_thread;
+
+    // 创建服务器socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Error creating server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(CONTROL_PORT);
+
+    // 绑定并监听
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error binding server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 5) < 0) {
+        perror("Error listening on server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("FTP Server started on port %d...\n", CONTROL_PORT);
+
+    // 接受客户端连接
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+        if (client_socket < 0) {
+            perror("Error accepting client connection");
+            continue;
+        }
+
+        printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+
+        // 启动新线程来处理客户端
+        pthread_create(&client_thread, NULL, handle_client, (void *) &client_socket);
+        pthread_detach(client_thread);
+    }
+}
+
+int main() {
+    start_ftp_server();
+    return 0;
+}
